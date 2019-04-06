dd3b927626b76e03b1875e77ee4c9861b4bb0eb3
bam-erfanian <erfanianbamdad@gmail.com>
2019-04-06 17:02:39 +1100

    filtering array

--- 09-wolf_in_sheeps_clothing/wolfInSheepsClothing.js
+++ 09-wolf_in_sheeps_clothing/wolfInSheepsClothing.js
@@ -1,12 +1,15 @@
 console.log('A Wolf in Sheeps Clothing')
 
-var sheeps = ["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"]
+var animal = ["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"]
 
-var wolfIndex = sheeps.indexOf("wolf")
+var newAnimal = animal.slice(0)
+var animalReveresed = newAnimal.reverse()
+var wolfIndex = animalReveresed.indexOf("wolf")
+
 
 var sheepInFrontOfWolf = wolfIndex - 1;
 
-if (sheeps[sheeps.length - 1] === "wolf") {
+if (animalReveresed[animalReveresed.length - 1] === "wolf") {
   console.log('Pls go away and stop eating my sheep')
 } else {
   console.log(`Oi! Sheep number ${sheepInFrontOfWolf + 1}! You are about to be eaten by a wolf!`)
---
+++ 10-filter_the_geese/app.js
@@ -0,0 +1,33 @@
+// Write a function, gooseFilter, that takes an array of strings as an argument and returns a filtered array containing the same elements but with the 'geese' removed.
+
+// The geese are any strings in the following array, which is pre-populated in your solution:
+// var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+// For example, if this array were passed as an argument:
+// ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+
+// Your function would return the following array:
+// ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+
+// The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+
+var words = ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+
+var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+var filtered = []
+
+for (i = 0; i < words.length; i++) {
+  for (j = 0; j < geese.length; j++) {
+    if (words[i] === geese[j]) {
+      filtered.push(words[i])
+    }
+  }
+}
+
+for (i = 0; i < filtered.length; i++) {
+  var indexOfWords = words.indexOf(filtered[i])
+  words.splice(indexOfWords, 1)
+}
+
+console.log(words)
