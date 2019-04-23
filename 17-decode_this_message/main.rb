71723de0979dceb01ab9026c8f49d905d9e11265
bam-erfanian <erfanianbamdad@gmail.com>
2019-04-23 20:14:06 +1000

    caesar cypher

---
+++ 17-decode_this_message/main.rb
@@ -0,0 +1,19 @@
+
+def decode_encode (message, shift_num)
+  message_chars = message.downcase.split("")
+  alphabet_chars = ("a".."z").to_a
+  crypto_chars = alphabet_chars.rotate(shift_num)
+  result_chars = []
+  message_chars.each do |char|
+    if crypto_chars.index(char) == nil
+      result_chars.push(char)
+    else
+      crypto_chars_index = crypto_chars.index(char)
+      result_chars.push(alphabet_chars[crypto_chars_index])
+    end
+  end
+  result = result_chars.join("")
+  return result.capitalize
+end
+
+p decode_encode("VITOCDIB, OCDN OCVO VIY RCVO", -5)
