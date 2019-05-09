f45e70e30d620aac73f270288a41a2190c40f3c7
bam-erfanian <erfanianbamdad@gmail.com>
2019-05-10 09:42:36 +1000

    Nested forEach Loop

---
+++ 27-odd_integer/app.js
@@ -0,0 +1,24 @@
+numbers = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]
+
+function FindOddInteger(arrayOfNums) {
+
+  arrayOfNums.forEach(function(number) {
+  
+    newArray = []
+    newArray.push(number)
+  
+    arrayOfNums.forEach(function(number) {
+      if (newArray[0] === number) {
+        newArray.push(number)
+      }
+    })
+    
+    if (newArray.length % 2 === 0) {
+      console.log(number)
+    }
+  
+  })
+
+}
+
+FindOddInteger(numbers)
